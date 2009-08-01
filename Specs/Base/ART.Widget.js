c1d31f3ee0544dfbc27a1f1a0caeb79deae04230
Thomas Aylott <oblivious+git@subtlegradient.com>
2009-07-31 22:48:50 -0400

    started spec for Widget

---
+++ Specs/Base/ART.Widget.js
@@ -0,0 +1,20 @@
+/*
+Script: ART.Widget.js
+
+License:
+	MIT-style license.
+
+Requires:
+	@require '../Assets/Scripts/jasmine.js'
+	@require '../Assets/Scripts/TrivialReporter.js'
+	@require '../Assets/Scripts/mootools.js'
+	@require '../../Source/Base/ART.js'
+	@require '../../Source/Base/ART.Widget.js'
+	@require '../Assets/Scripts/jasmine-run.js'
+*/
+
+describe('ART.Widget', function(){
+	it('should exist', function(){
+		expect(ART.Widget).toBeDefined();
+	});
+});
