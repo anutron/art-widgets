5dc0c52abaae164835d92156b0d9738aebdb2dbb
Thomas Aylott <oblivious+git@subtlegradient.com>
2009-08-01 00:49:31 -0400

    beginning spec of ART.Widget

--- Specs/Base/ART.Widget.js
+++ Specs/Base/ART.Widget.js
@@ -11,10 +11,39 @@ Requires:
 	@require '../../Source/Base/ART.Widget.js'
 */
 
+var w;
+var container;
+
 describe('ART.Widget', {
 	
-	'should exist': function(){
+	before_all : function(){
+		container = new Element('div')
+			.inject(document.body)
+			.setStyles({
+				position:'absolute'
+				,bottom:'0px'
+				,right:'0px'
+				,zIndex:'9999'
+				,backgroundColor:'#fff'
+			})
+		;
+	}
+	,before : function(){
+		w = new ART.Widget();
+	}
+	,after : function(){
+		container.empty();
+		$(w).dispose();
+		w = null;
+	}
+	
+	,'should exist': function(){
 		value_of(ART.Widget).should_not_be_undefined();
+	}
+	
+	,'should implement toElement': function(){
+		$(w).inject(container);
+		value_of( container.contains($(w)) ).should_be_true();
 	}
 	
 });
