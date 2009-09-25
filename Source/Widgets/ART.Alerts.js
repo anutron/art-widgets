4f24668471b32c4c7e8055e37c76a9b92c09f8e9
Aaron Newton <git@clientcide.com>
2009-09-25 13:27:45 -0700

    updating -more adding ART.Alerts.js, ART.SolidWindow.js - still need to work on these a bit.

---
+++ Source/Widgets/ART.Alerts.js
@@ -0,0 +1,88 @@
+/*
+Script: ART.Alerts.js
+
+License:
+	MIT-style license.
+*/
+
+// Window Widget. Work in progress.
+
+ART.Sheet.defineStyle('window.alert', {
+	'content-background-color': hsb(0, 0, 91),
+	'footer-height': 30,
+	'height': 140,
+	'width': 300,
+	'border-color': hsb(0, 0, 80),
+	'footer-background-color': hsb(0, 0, 91),
+	'footer-reflection-color': hsb(0, 0, 91),
+	'content-border-bottom-color': hsb(0, 0, 91)
+});
+
+ART.Sheet.defineStyle('window.alert content', {
+	'padding': 20,
+	'font-size': 14,
+	'text-align': 'center'
+});
+
+ART.StickyWin.DefaultManager.setLayer('alerts', 99);
+
+ART.Alert = new Class({
+	
+	Extends: ART.Window,
+	
+	name: 'window',
+	
+	options: {
+		close: false,
+		minimize: false,
+		maximize: false,
+		className: 'alert',
+		resizable: false,
+		windowManagerLayer: 'alerts',
+		buttons: [
+			{
+				text: 'Ok'
+			}
+		]
+	},
+
+	render: function(){
+		this.parent();
+		if (!this.content) return;
+		var style = ART.Sheet.lookupStyle(this.getSelector() + ' content');
+		if (style.margin || style.padding) {
+			var tmp = new Element('div', {styles: style});
+			var w = 0,
+					h = 0;
+			['margin', 'padding'].each(function(space) {
+				['Top', 'Left', 'Bottom', 'Right'].each(function(side, i) {
+					if (i%2) w += tmp.getStyle(space+side).toInt();
+					else h += tmp.getStyle(space+side).toInt();
+				});
+			});
+			style.width = this.contentSize.w - w; //border is hard coded to 1 on each side
+			style.height = this.contentSize.h - h; //border is hard coded to 1 on each side
+		}
+		this.content.setStyles(style);
+	},
+
+	makeButtons: function(){
+		this.parent();
+		this.options.buttons.each(function(button){
+			var b = new ART.Button(button).setParent(this);
+			b.addEvent('activate', function(){
+				if ($(b).hasClass(this.options.closeClass)) this.hide();
+			}.bind(this));
+
+			button.properties = button.properties || {};
+			if (!button.properties['class']) button.properties['class'] = this.options.closeClass;
+			$(b).set(button.properties);
+
+			$(b).inject(this.footer).setStyles({
+				'float': 'right',
+				'padding-right': 10
+			});
+		}, this);
+	}
+
+});
---
+++ Source/Widgets/ART.SolidWindow.js
@@ -0,0 +1,161 @@
+/*
+Script: ART.SolidWindow.js
+
+License:
+	MIT-style license.
+*/
+
+// Window Widget. Work in progress.
+
+ART.Sheet.defineStyle('window.solid', {
+	'content-overflow': 'hidden',
+	'body-background-color': {0: hsb(0, 0, 95), 1: hsb(0, 0, 80)},
+	'body-reflection-color': {0: hsb(0, 0, 100, 1), 1: hsb(0, 0, 0, 0)},
+	'body-reflection-shape': 'rounded-rectangle'
+});
+
+ART.Sheet.defineStyle('window.solid.smoke', {
+	'caption-font-color': hsb(94, 82, 100),
+	'body-background-color': {0: hsb(0, 0, 0, 0.8), 1: hsb(0, 0, 0, 0.5)},
+	'body-reflection-color': {0: hsb(0, 0, 100, 0.1), 1: hsb(0, 0, 100, 0.2)},
+	'body-reflection-percent-size': 0.6,
+	'content-color': hsb(94, 82, 100),
+	'body-reflection-shape': 'funky-glass'
+});
+
+
+ART.Sheet.defineStyle('window.solid.smoke button.wincontrol', {
+	'pill': true,
+	'height': 14,
+	'width': 14,
+	'cursor': 'pointer',
+	'background-color': hsb(0, 0, 100, 0),
+	'reflection-color': hsb(0, 0, 100, 0),
+	'shadow-color': hsb(0, 0, 100, 0),
+	'border-color': hsb(0, 0, 100, 0),
+	'glyph-color': hsb(94, 82, 100)
+});
+
+ART.Sheet.defineStyle('window.solid:focus button.wincontrol', {
+	'background-color': {0: hsb(0, 0, 80), 1: hsb(0, 0, 70)},
+	'reflection-color': {0: hsb(0, 0, 95), 1: hsb(0, 0, 0, 0)},
+	'shadow-color': hsb(0, 0, 100, 0.7),
+	'border-color': hsb(0, 0, 60),
+	'glyph-color': hsb(0, 0, 0, 0.6)
+});
+
+ART.Sheet.defineStyle('window.solid.smoke:focus button.wincontrol', {
+	'background-color': hsb(0, 0, 100, 0),
+	'reflection-color': hsb(0, 0, 100, 0),
+	'shadow-color': hsb(0, 0, 100, 0),
+	'border-color': hsb(0, 0, 100, 0),
+	'glyph-color': hsb(94, 82, 100)
+});
+
+ART.Sheet.defineStyle('window.solid.smoke button.wincontrol:active', {
+	'background-color': hsb(0, 0, 100, 0),
+	'reflection-color': hsb(0, 0, 100, 0),
+	'shadow-color': hsb(0, 0, 100, 0),
+	'border-color': hsb(0, 0, 100, 0),
+	'glyph-color': hsb(0, 0, 100, 1)
+});
+
+ART.SolidWindow = new Class({
+	
+	Extends: ART.Window,
+	
+	name: 'window',
+
+	options: {
+		className: 'solid'
+	},
+
+	renderContent: function(style){
+		var contentHeight = style.height - style.footerHeight - style.headerHeight - 2;
+		var contentWidth = style.width -2;
+		this.contentSize = {
+			w: contentWidth, 
+			h: contentHeight
+		};
+		this.content.setStyles({
+			'top': 1,
+			'left': 0,
+			'height': contentHeight < 0 ? 0 : contentHeight,
+			'width': contentWidth < 0 ? 0 : contentWidth,
+			'overflow': style.contentOverflow,
+			'color': style.contentColor
+		});
+
+		// border layer
+		this.paint.save();
+
+		if (this.options.shadow) this.paint.shift({x: 10, y: 5});
+
+		this.paint.start();
+		this.paint.shape('rounded-rectangle', {x: style.width, y: style.height}, style.cornerRadius + 1);
+
+		var border = {'fill': true, 'fill-color': style.borderColor};
+
+		if (this.options.shadow) $mixin(border, {
+			'shadow-color': hsb(0, 0, 0),
+			'shadow-blur': 8,
+			'shadow-offset-x': 0,
+			'shadow-offset-y': 5
+		});
+
+		this.paint.end(border);
+
+		// header layers
+
+		this.header.setStyles({'width': style.width - 2, height: style.headerHeight - 2});
+
+		this.paint.start({x: 1, y: 2});
+		this.paint.shape('rounded-rectangle', {x: style.width - 2, y: style.height - 3}, style.cornerRadius);
+		this.paint.end({'fill': true, 'fill-color': style.bodyBackgroundColor});
+
+		this.paint.start({x: 1, y: 1});
+
+		style.bodyReflectionPercentSize = style.bodyReflectionPercentSize || 1;
+		this.paint.shape(style.bodyReflectionShape, {
+			x: style.width - 2, 
+			y: (style.bodyReflectionPercentSize * style.height) - 2
+		}, style.cornerRadius);
+
+		
+		this.paint.end({'fill': true, 'fill-color': style.bodyReflectionColor});
+
+		//footer layers
+
+		this.footer.setStyles({'width': style.width - 2, 'height': style.footerHeight});
+
+	}
+
+});
+
+ART.Paint.defineShapes({
+	'funky-glass': function(size, radius){
+		size = this.getXY(size);
+		if (radius == null) radius = [5, 5];
+		if (typeof radius == 'number') radius = [radius, radius];
+		
+		var tl = radius[0], tr = radius[1];
+		
+		this.moveBy({x: 0, y: tl});
+		
+		if (size.x < 0) this.moveBy({x: size.x, y: 0});
+		if (size.y < 0) this.moveBy({x: 0, y: size.y});
+		
+		if (tl > 0) this.roundCapLeftBy({x: tl, y: -tl});
+		this.lineBy({x: Math.abs(size.x) - (tr + tl), y: 0});
+		
+		if (tr > 0) this.roundCapRightBy({x: tr, y: tr});
+		this.lineBy({x: 0, y: Math.abs(size.y * 0.5) - tr});
+
+		this.roundCapRightBy({x: - size.x, y: size.y - (size.y * 0.5)});
+
+
+		this.lineBy({x: 0, y: - Math.abs(size.y * 0.5) + tl});
+		
+		this.moveBy({x: size.x, y: -tl + size.y});
+	}
+});
