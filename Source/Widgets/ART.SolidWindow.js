37c31bf69f6963ced92fc6c272f7bbbee5d143ea
Valerio Proietti <kamicane@gmail.com>
2010-03-03 02:59:08 +0100

    - added back the funky SolidWindow Window Vista style reflection.

--- Source/Widgets/ART.SolidWindow.js
+++ Source/Widgets/ART.SolidWindow.js
@@ -9,17 +9,17 @@ License:
 
 ART.Sheet.defineStyle('window.solid', {
 	'content-overflow': 'hidden',
-	'body-background-color': [Color.hsb(0, 0, 95), Color.hsb(0, 0, 80)],
-	'body-reflection-color': [Color.hsb(0, 0, 100, 1), Color.hsb(0, 0, 0, 0)],
+	'body-background-color': [hsb(0, 0, 95), hsb(0, 0, 80)],
+	'body-reflection-color': [hsb(0, 0, 100, 1), hsb(0, 0, 0, 0)],
 	'body-reflection-shape': 'rounded-rectangle'
 });
 
 ART.Sheet.defineStyle('window.solid.smoke', {
-	'caption-font-color': Color.hsb(120, 97, 83),
-	'body-background-color': [Color.hsb(0, 0, 0, 0.9), Color.hsb(0, 0, 0, 0.8)],
-	'body-reflection-color': [Color.hsb(0, 0, 100, 0.05), Color.hsb(0, 0, 100, 0.1)],
+	'caption-font-color': hsb(120, 97, 83),
+	'body-background-color': [hsb(0, 0, 0, 0.9), hsb(0, 0, 0, 0.8)],
+	'body-reflection-color': [hsb(0, 0, 100, 0.05), hsb(0, 0, 100, 0.1)],
 	'body-reflection-percent-size': 0.6,
-	'content-color': Color.hsb(120, 97, 83),
+	'content-color': hsb(120, 97, 83),
 	'body-reflection-shape': 'funky-glass'
 });
 
@@ -29,35 +29,35 @@ ART.Sheet.defineStyle('window.solid.smoke button.wincontrol', {
 	'height': 14,
 	'width': 14,
 	'cursor': 'pointer',
-	'background-color': Color.hsb(0, 0, 100, 0),
-	'reflection-color': Color.hsb(0, 0, 100, 0),
-	'shadow-color': Color.hsb(0, 0, 100, 0),
-	'border-color': Color.hsb(0, 0, 100, 0),
-	'glyph-color': Color.hsb(120, 97, 83)
+	'background-color': hsb(0, 0, 100, 0),
+	'reflection-color': hsb(0, 0, 100, 0),
+	'shadow-color': hsb(0, 0, 100, 0),
+	'border-color': hsb(0, 0, 100, 0),
+	'glyph-color': hsb(120, 97, 83)
 });
 
 ART.Sheet.defineStyle('window.solid button.wincontrol', {
-	'background-color': [Color.hsb(0, 0, 80), Color.hsb(0, 0, 70)],
-	'reflection-color': [Color.hsb(0, 0, 95), Color.hsb(0, 0, 0, 0)],
-	'shadow-color': Color.hsb(0, 0, 100, 0.7),
-	'border-color': Color.hsb(0, 0, 60),
-	'glyph-color': Color.hsb(0, 0, 0, 0.6)
+	'background-color': [hsb(0, 0, 80), hsb(0, 0, 70)],
+	'reflection-color': [hsb(0, 0, 95), hsb(0, 0, 0, 0)],
+	'shadow-color': hsb(0, 0, 100, 0.7),
+	'border-color': hsb(0, 0, 60),
+	'glyph-color': hsb(0, 0, 0, 0.6)
 });
 
 ART.Sheet.defineStyle('window.solid.smoke button.wincontrol:disabled', {
-	'background-color': Color.hsb(0, 0, 100, 0),
-	'reflection-color': Color.hsb(0, 0, 100, 0),
-	'shadow-color': Color.hsb(0, 0, 100, 0),
-	'border-color': Color.hsb(0, 0, 100, 0),
-	'glyph-color': Color.hsb(120, 97, 83)
+	'background-color': hsb(0, 0, 100, 0),
+	'reflection-color': hsb(0, 0, 100, 0),
+	'shadow-color': hsb(0, 0, 100, 0),
+	'border-color': hsb(0, 0, 100, 0),
+	'glyph-color': hsb(120, 97, 83)
 });
 
 ART.Sheet.defineStyle('window.solid.smoke button.wincontrol:active', {
-	'background-color': Color.hsb(0, 0, 100, 0),
-	'reflection-color': Color.hsb(0, 0, 100, 0),
-	'shadow-color': Color.hsb(0, 0, 100, 0),
-	'border-color': Color.hsb(0, 0, 100, 0),
-	'glyph-color': Color.hsb(0, 0, 100, 1)
+	'background-color': hsb(0, 0, 100, 0),
+	'reflection-color': hsb(0, 0, 100, 0),
+	'shadow-color': hsb(0, 0, 100, 0),
+	'border-color': hsb(0, 0, 100, 0),
+	'glyph-color': hsb(0, 0, 100, 1)
 });
 
 ART.SolidWindow = new Class({
@@ -68,6 +68,12 @@ ART.SolidWindow = new Class({
 
 	options: {
 		className: 'solid'
+	},
+	
+	build: function(){
+		this.parent();
+		this.vistaReflectionLayer = new ART.FunkyGlass;
+		this.vistaReflectionLayer.inject(this.paint);
 	},
 
 	renderContent: function(style){
@@ -90,6 +96,7 @@ ART.SolidWindow = new Class({
 				'display': 'block'
 			});
 		}
+
 		// border layer
 		
 		this.borderLayer.draw(style.width, style.height, style.cornerRadius + 1);
@@ -98,13 +105,20 @@ ART.SolidWindow = new Class({
 		// header layers
 
 		this.header.setStyles({'width': style.width - 2, height: style.headerHeight - 2});
+		
 		this.headerBackgroundLayer.translate(1, 2);
 		this.headerBackgroundLayer.draw(style.width - 2, style.height - 3, style.cornerRadius);
 		this.fill(this.headerBackgroundLayer, style.bodyBackgroundColor);
 		
 		this.headerReflectionLayer.translate(1, 1);
 		this.headerReflectionLayer.draw(style.width - 2, style.headerHeight - 2, style.cornerRadius);
-		this.fill(this.headerReflectionLayer, style.bodyReflectionColor);
+		this.fill(this.headerReflectionLayer, style.headerReflectionColor);
+		
+		
+		// funky vista reflection
+		style.bodyReflectionPercentSize = style.bodyReflectionPercentSize || 1;
+		this.vistaReflectionLayer.translate(1,1).draw(style.width - 2, (style.bodyReflectionPercentSize * style.height), style.cornerRadius);
+		this.fill(this.vistaReflectionLayer, style.bodyReflectionColor);
 
 		//footer layers
 
@@ -114,32 +128,45 @@ ART.SolidWindow = new Class({
 
 });
 
-// ## I have no idea what this is
+// ## This is a reflection, glossy windows vista style. Not for the faint of heart.
 
-// ART.Paint.defineShapes({
-// 	'funky-glass': function(size, radius){
-// 		size = this.getXY(size);
-// 		if (radius == null) radius = [5, 5];
-// 		if (typeof radius == 'number') radius = [radius, radius];
-// 		
-// 		var tl = radius[0], tr = radius[1];
-// 		
-// 		this.moveBy({x: 0, y: tl});
-// 		
-// 		if (size.x < 0) this.moveBy({x: size.x, y: 0});
-// 		if (size.y < 0) this.moveBy({x: 0, y: size.y});
-// 		
-// 		if (tl > 0) this.roundCapLeftBy({x: tl, y: -tl});
-// 		this.lineBy({x: Math.abs(size.x) - (tr + tl), y: 0});
-// 		
-// 		if (tr > 0) this.roundCapRightBy({x: tr, y: tr});
-// 		this.lineBy({x: 0, y: Math.abs(size.y * 0.5) - tr});
-// 
-// 		this.roundCapRightBy({x: - size.x, y: size.y - (size.y * 0.5)});
-// 
-// 
-// 		this.lineBy({x: 0, y: - Math.abs(size.y * 0.5) + tl});
-// 		
-// 		this.moveBy({x: size.x, y: -tl + size.y});
-// 	}
-// });
+ART.FunkyGlass = new Class({
+	
+	Extends: ART.Shape,
+	
+	initialize: function(width, height, radius){
+		this.parent();
+		if (width != null && height != null && radius != null) this.draw(width, height, radius);
+	},
+	
+	draw: function(width, height, radius){
+
+		if (radius == null) radius = [5, 5];
+		if (typeof radius == 'number') radius = [radius, radius];
+		var tl = radius[0], tr = radius[1];
+		
+		var path = new ART.Path;
+		
+		path.move(0, tl);
+		
+		if (width < 0) path.move(width, 0);
+		if (height < 0) path.move(0, height);
+		
+		if (tl > 0) path.arcLeft(tl, -tl);
+		path.line(Math.abs(width) - (tr + tl), 0);
+		
+		if (tr > 0) path.arcRight(tr, tr);
+		path.line(0, Math.abs(height * 0.5));
+
+		path.arcRight(-width, height - (height * 0.5));
+
+
+		path.line(0, - Math.abs(height * 0.5) + tl);
+		
+		path.move(width, -tl + height);
+		
+		this.parent(path);
+		
+	}
+	
+});
