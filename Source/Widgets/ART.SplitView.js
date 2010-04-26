bfe39eae53b1d56ae4e8cf936cfdb41e15c12908
Aaron Newton <git@clientcide.com>
2010-04-26 16:22:19 -0700

    implementing the new API into splitview

--- Source/Widgets/ART.SplitView.js
+++ Source/Widgets/ART.SplitView.js
@@ -7,7 +7,7 @@ provides: ART.SplitView
 ...
 */
 
-ART.Sheet.defineStyle('splitview', {
+ART.Sheet.define('splitview.art', {
 	'width': 600,
 	'height': 400,
 	'fixed-width': 200,
@@ -23,7 +23,7 @@ ART.Sheet.defineStyle('splitview', {
 	'right-background-color': '#fff'
 });
 
-ART.Sheet.defineStyle('splitview:disabled', {
+ART.Sheet.define('splitview.art:disabled', {
 	'splitter-background-color': hsb(0, 0, 70),
 	'left-background-color': '#e8e8e8'
 });
@@ -44,19 +44,40 @@ ART.SplitView = new Class({
 	name: 'splitview',
 	
 	initialize: function(options){
-		this.requireToRender('splitview:panes');
 		this.parent(options);
-		this.build();
+		this._build();
 	},
 
-	build: function(){
-		var style = ART.Sheet.lookupStyle(this.getSelector());
+	_build: function(){
+		document.id(this.canvas).dispose();
+		var sheet = this.setSheet();
 
-		this.element.addClass('art-splitview').setStyles({'position': 'relative'});
+		this.element.addClass('art-splitview').setStyles({
+			'position': 'relative',
+			'width': sheet.width,
+			'height': sheet.height
+		});
 		var styles = {'float': 'left', 'overflow-x': 'auto'};
-		this.left = new Element('div', {'class': 'art-splitview-left'}).inject(this.element).setStyles(styles);
-		this.splitter = new Element('div', {'class': 'art-splitview-splitter'}).inject(this.element).setStyles(styles);
-		this.right = new Element('div', {'class': 'art-splitview-right'}).inject(this.element).setStyles(styles);
+		
+		this.left = new Element('div', {
+			'class': 'art-splitview-left',
+			styles: {
+				'background-color': sheet.leftBackgroundColor
+			}
+		}).inject(this.element).setStyles(styles);
+		this.splitter = new Element('div', {
+			'class': 'art-splitview-splitter',
+			styles: {
+				'width': sheet.splitterWidth,
+				'background-color': sheet.splitterBackgroundColor
+			}
+		}).inject(this.element).setStyles(styles);
+		this.right = new Element('div', {
+			'class': 'art-splitview-right',
+			styles: {
+				'background-color': sheet.rightBackgroundColor
+			}
+		}).inject(this.element).setStyles(styles);
 		
 		this.fx = new Fx();
 		this.touch = new Touch(this.splitter);
@@ -70,8 +91,11 @@ ART.SplitView = new Class({
 			});
 		}
 
-		if (this.options.resizable) this.touch.addEvent('move', this.moveSplitter.bind(this));
-
+		if (this.options.resizable) {
+			this.touch.addEvent('move', function(){
+				this.moveSplitter.apply(this, arguments);
+			}.bind(this));
+		}
 		if (this.options.foldable){
 			this.touch.addEvent('cancel', function(){
 				if (self[fix + 'Width'] == 0){
@@ -83,82 +107,90 @@ ART.SplitView = new Class({
 			});
 		}
 		
-		this.readyToRender('splitview:panes');
-		this.render();
-		this['resize' + this.options.fixed.capitalize()](style.fixedWidth);
+		this.deferDraw();
 	},
 
 	moveSplitter: function(dx){
+		var cs = this.currentSheet;
 		var targetWidth = this.startFixWidth + dx;
 		if (targetWidth < 0) targetWidth = 0;
-		else if (targetWidth > this.currentWidth - this.splitterWidth) targetWidth = this.currentWidth - this.splitterWidth;
+		else if (targetWidth > cs.width - cs.splitterWidth) targetWidth = cs.width - cs.splitterWidth;
 		var fix = this.options.fixed.capitalize();
 		this['resize' + fix](targetWidth);
 	},
 
-	redraw: function(override){
-		var style = ART.Sheet.lookupStyle(this.getSelector());
+	draw: function(newSheet){
+		var cs = this.currentSheet;
+		var sheet = this.parent(newSheet);
+		
+		var sizeChanged = (sheet.width != undefined && sheet.height != undefined);
 
-		this.currentHeight = $pick(this.currentHeight, style.height);
-		this.currentWidth = $pick(this.currentWidth, style.width);
-		this.splitterWidth = style.splitterWidth;
+		if (sizeChanged) {
+			this.currentHeight = cs.height;
+			this.currentWidth = cs.width;
 		
+			// render
 		
-		// height / width management
+			this.element.setStyles({
+				'width': cs.width,
+				'height': cs.height
+			});
+		}
 		
-		delete style.height;
-		delete style.width;
+		if (true || sheet.splitterWidth != undefined) {
+			cs.splitterWidth = cs.splitterWidth;
+			this.splitter.setStyles({
+				'width': cs.splitterWidth,
+				'background-color': cs.splitterBackgroundColor
+			});
+		}
+		if (this.options.resizable) this.splitter.setStyle('cursor', cs.splitterCursor);
+		if (true || sheet.leftBackgroundColor)
+			this.left.setStyles({'background-color': cs.leftBackgroundColor});
+		if (true || sheet.rightBackgroundColor)
+			this.right.setStyles({'background-color': cs.rightBackgroundColor});
 		
-		$mixin(style, override);
-		if (style.height == null) style.height = this.currentHeight;
-		if (style.width == null) style.width = this.currentWidth;
+		if (sizeChanged) {
+			$$(this.left, this.right, this.splitter).setStyle('height', cs.height);
 		
-		this.currentHeight = style.height;
-		this.currentWidth = style.width;
+			var side = this.options.fixed;
 		
-		// render
-		
-		this.element.setStyles({'width': this.currentWidth, 'height': this.currentHeight});
-		
-		this.splitter.setStyles({'width': style.splitterWidth, 'background-color': style.splitterBackgroundColor});
-		if (this.options.resizable) this.splitter.setStyle('cursor', style.splitterCursor);
-		this.left.setStyles({'background-color': style.leftBackgroundColor});
-		this.right.setStyles({'background-color': style.rightBackgroundColor});
-		
-		$$(this.left, this.right, this.splitter).setStyle('height', this.currentHeight);
-		
-		var side = this.options.fixed;
-		
-		if (side == 'left'){
-			this.resizeRight(this.currentWidth - this.leftWidth - style.splitterWidth);
-		} else if (side == 'right'){
-			this.resizeLeft(this.currentWidth - this.rightWidth - style.splitterWidth);
+			if (side == 'left'){
+				if (this.leftWidth == undefined) this.leftWidth = cs.fixedWidth;
+				this.resizeRight(cs.width - this.leftWidth - cs.splitterWidth);
+			} else if (side == 'right'){
+				if (this.leftRight == undefined) this.leftRight = cs.fixedWidth;
+				this.resizeLeft(cs.width - this.rightWidth - cs.splitterWidth);
+			}
 		}
 		
 		return this;
 	},
 	
 	resize: function(w, h){
-		return this.render({'height': h, 'width': w});
+		return this.draw({'height': h, 'width': w});
 	},
 	
 	resizeLeft: function(width){
-		width = width.limit(0, this.currentWidth - this.splitterWidth);
+		var cs = this.currentSheet;
+		width = width.limit(0, cs.width - cs.splitterWidth);
 		this.left.setStyle('width', width);
 		this.leftWidth = width;
-		this.rightWidth = this.currentWidth - this.splitterWidth - width;
+		this.rightWidth = cs.width - cs.splitterWidth - width;
 		this.right.setStyle('width', this.rightWidth);
 	},
 	
 	resizeRight: function(width){
-		width = width.limit(0, this.currentWidth - this.splitterWidth);
+		var cs = this.currentSheet;
+		width = width.limit(0, cs.width - cs.splitterWidth);
 		this.right.setStyle('width', width);
 		this.rightWidth = width;
-		this.leftWidth = this.currentWidth - this.splitterWidth - width;
+		this.leftWidth = cs.width - cs.splitterWidth - width;
 		this.left.setStyle('width', this.leftWidth);
 	},
 	
 	fold: function(side, to, hideSplitter) {
+		var cs = this.currentSheet;
 		hideSplitter = $pick(hideSplitter, this.options.hideSplitterOnFullFold);
 		var self = this;
 		var other = side == 'left' ? 'right' : 'left';
@@ -166,9 +198,8 @@ ART.SplitView = new Class({
 			self['resize' + side.capitalize()](now);
 		};
 		if (to > 0 && this[side + 'Width'] && this.splitterHidden) {
-			var style = ART.Sheet.lookupStyle(self.getSelector());
-			self.splitter.setStyle('width', style.splitterWidth);
-			self[other].setStyle('width', self[other + 'Width'] - style.splitterWidth);
+			self.splitter.setStyle('width', cs.splitterWidth);
+			self[other].setStyle('width', self[other + 'Width'] - cs.splitterWidth);
 			this.splitterHidden = false;
 		}
 		this.fx.start(this[side + 'Width'], to).chain(function(){
@@ -176,10 +207,9 @@ ART.SplitView = new Class({
 				['left', 'right'].each(function(side) {
 					var other = side == 'left' ? 'right' : 'left';
 					if (self[side + 'Width'] == 0) {
-						var style = ART.Sheet.lookupStyle(self.getSelector());
 						self.splitter.setStyle('width', 0);
-						self[other].setStyle('width', self[other + 'Width'] + style.splitterWidth);
-						self[other + 'Width'] = self[other + 'Width'] + style.splitterWidth;
+						self[other].setStyle('width', self[other + 'Width'] + cs.splitterWidth);
+						self[other + 'Width'] = self[other + 'Width'] + cs.splitterWidth;
 					}
 				});
 				this.splitterHidden = true;
@@ -190,12 +220,12 @@ ART.SplitView = new Class({
 	},
 
 	setLeftContent: function(){
-		$(this.left).adopt(arguments);
+		document.id(this.left).empty().adopt(arguments);
 		return this;
 	},
 	
 	setRightContent: function(){
-		$(this.right).adopt(arguments);
+		document.id(this.right).empty().adopt(arguments);
 		return this;
 	}
 
