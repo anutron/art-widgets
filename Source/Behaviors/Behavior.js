3807cf0f63a924155afc89f39a45928d477e0e25
Aaron Newton <git@clientcide.com>
2010-10-25 14:27:41 -0700

    adding window tools to Behavior.Filter

---
+++ Source/Behaviors/Behavior.js
@@ -0,0 +1,9 @@
+/*
+---
+name: ART.Behavior
+description: Implements ART.WindowTools into Behavior.Filter.
+requires: [/ART.Window, Behavior/Behavior]
+provides: [Behavior.WindowTools]
+...
+*/
+Behavior.Filter.implement(ART.WindowTools);
